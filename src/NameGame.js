5775ce4654b55ae562e9e50033613701b0088cfe
Brett Hevia <brett.hevia@gmail.com>
2016-11-17 15:00:21 -0500

    randomly selects five different people, displays their pictures, and displays the correct answer name

--- src/NameGame.js
+++ src/NameGame.js
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { shuffle } from 'lodash';
 
 export default class NameGame extends Component {
   constructor() {
     super()
     this.state = {
-      willowTreePeople: []
+      willowTreePeople: [],
+      randomPeopleForGameRound: [],
+      answerPerson: {}
     }
+  }
+
+  getRandomFromArray(array, number) {
+    let newArray = shuffle(array)
+    return newArray.slice(0, number)
   }
 
   getWillowTreePeople() {
     axios.get('http://api.namegame.willowtreemobile.com/')
     .then((response)=> {
       this.setState({ willowTreePeople: response.data })
+      this.setState({ randomPeopleForGameRound: this.getRandomFromArray( response.data, 5 )})
+      this.setState({ answerPerson: this.getRandomFromArray( this.state.randomPeopleForGameRound, 1 )[0] })
     })
     .catch(()=> {
       console.log('error')
     })
   }
 
+  displayRoundPictures() {
+    return this.state.randomPeopleForGameRound.map((person, index) => <img key={index} src={person.url}/>)
+  }
+
+
   componentWillMount() {
-    this.getWillowTreePeople()
+    this.getWillowTreePeople();
   }
 
-  displayNames() {
-    return this.state.willowTreePeople.map((person, index) => <li key={index}>{person.name}</li>)
-  }
-
-  displayPicture(i) {
-    return <img src={this.state.willowTreePeople[i].url}/>
-  }
 
   render() {
+    const { answerPerson, randomPeopleForGameRound } = this.state
     return(
       <section className="name-game">
-        { this.state.willowTreePeople.length > 0 ? this.displayPicture(Math.floor(Math.random()*this.state.willowTreePeople.length)) : null }
-        <ul>
-          { this.state.willowTreePeople ? this.displayNames() : null}
-        </ul>
+        <h2> { answerPerson ? answerPerson.name : null } </h2>
+        { randomPeopleForGameRound ? this.displayRoundPictures() : null }
       </section>
     )
   }
