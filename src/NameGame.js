acb819c2b6b9760da939bafd933afeb7da3680ab
Brett Hevia <brett.hevia@gmail.com>
2016-11-17 20:01:56 -0500

    displays correct or wrong based on user's choice

--- src/NameGame.js
+++ src/NameGame.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { shuffle } from 'lodash';
+import Picture from './Picture';
 
 export default class NameGame extends Component {
   constructor() {
@@ -8,8 +9,13 @@ export default class NameGame extends Component {
     this.state = {
       willowTreePeople: [],
       randomPeopleForGameRound: [],
-      answerPerson: {}
+      answerPerson: {},
+      lastGuess: null
     }
+  }
+
+  componentWillMount() {
+    this.getWillowTreePeople();
   }
 
   getRandomFromArray(array, number) {
@@ -21,30 +27,41 @@ export default class NameGame extends Component {
     axios.get('http://api.namegame.willowtreemobile.com/')
     .then((response)=> {
       this.setState({ willowTreePeople: response.data })
-      this.setState({ randomPeopleForGameRound: this.getRandomFromArray( response.data, 5 )})
-      this.setState({ answerPerson: this.getRandomFromArray( this.state.randomPeopleForGameRound, 1 )[0] })
+      this.setRound(response.data)
     })
     .catch(()=> {
       console.log('error')
     })
   }
 
-  displayRoundPictures() {
-    return this.state.randomPeopleForGameRound.map((person, index) => <img key={index} src={person.url}/>)
+  setRound(peopleArray) {
+    this.setState({ randomPeopleForGameRound: this.getRandomFromArray( peopleArray, 5 )})
+    this.setState({ answerPerson: this.getRandomFromArray( this.state.randomPeopleForGameRound, 1 )[0] })
   }
 
-
-  componentWillMount() {
-    this.getWillowTreePeople();
+  checkGuess(name) {
+    if (name === this.state.answerPerson.name) { this.setState({ lastGuess: 'Correct' }) }
+    else { this.setState({ lastGuess: 'Wrong' })}
   }
 
+  displayPictures() {
+    return this.state.randomPeopleForGameRound.map((person, index) => <Picture key={index} checkGuess={(name)=>this.checkGuess(name)} {...person} />)
+  }
 
   render() {
-    const { answerPerson, randomPeopleForGameRound } = this.state
+    console.log(this.state.lastGuess)
+    const { answerPerson, randomPeopleForGameRound, lastGuess } = this.state;
+
     return(
+
       <section className="name-game">
+
         <h2> { answerPerson ? answerPerson.name : null } </h2>
-        { randomPeopleForGameRound ? this.displayRoundPictures() : null }
+
+        { randomPeopleForGameRound ? this.displayPictures() : null }
+
+        <h2> { lastGuess ? lastGuess : null} </h2>
+
       </section>
     )
   }
